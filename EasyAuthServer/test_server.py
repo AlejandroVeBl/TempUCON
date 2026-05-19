6471c3802f8795198f1ff726f9f2b4741f8f6236
Alejandro Vera Blanco <alefvebl@gmail.com>
2026-05-19 16:21:53 +0200

    Added the EasyAuth test server so it's also on github

---
+++ EasyAuthServer/test_server.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+from easyauth.server import EasyAuthServer
+
+server = FastAPI()
+
+server.auth = EasyAuthServer.create(
+    server,
+    '/auth/token',
+    auth_secret='abcd1234',
+    admin_title='EasyAuth - Company',
+    admin_prefix='/admin',
+    env_from_file='server_env.json'
+)
